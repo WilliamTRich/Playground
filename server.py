6c90fd5dccd2f1dd67e4fc1ce76bc7b8b2a83a55
WilliamTRich <112077180+WilliamTRich@users.noreply.github.com>
2022-10-02 21:48:48 -0700

    Staging Project

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def index():
+    return render_template('index.html', box=0)
+
+@app.route('/play')
+def play():
+    return render_template('index.html', box=3)
+@app.route('/play/<x>')
+def playX(x):
+    return render_template('index.html', box=int(x))
+@app.route('/play/<x>/<y>')
+def playY(x, y):
+    return render_template('index.html', box=int(x), color=y)
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)  
